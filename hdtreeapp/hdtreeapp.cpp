923d3f888894265d28821d5798a1a17431e09d4d
asm128 <pabloarielz@outlook.com>
2023-06-05 00:40:00 -0300

    + Added functions for loading the tree view images.

--- hdtreeapp/hdtreeapp.cpp
+++ hdtreeapp/hdtreeapp.cpp
@@ -5,24 +5,220 @@
 using namespace hd;
 
 static void calcTreeRect(const RECT & rcClient, RECT & rcTree) { 
-	rcTree = {MARGIN_SIZE.x, MARGIN_SIZE.y, rcClient.right - MARGIN_SIZE.x, long(rcClient.bottom - BUTTON_SIZE.y - MARGIN_SIZE.y * 2)}; 
+	rcTree = {SIZE_MARGIN.x, SIZE_MARGIN.y, rcClient.right - SIZE_MARGIN.x, long(rcClient.bottom - SIZE_BUTTON.y - SIZE_MARGIN.y * 2)}; 
 }
 
 static void calcButtonsPosition(const RECT & rcClient, n2u32 & xCat, n2u32 & xMat, n2u32 & xClr) {
-	xCat.x = MARGIN_SIZE.x;
-	xMat.x = (rcClient.right >> 1) - (BUTTON_SIZE.x >> 1);
-	xClr.x = rcClient.right - MARGIN_SIZE.x - BUTTON_SIZE.x;
+	xCat.x = SIZE_MARGIN.x;
+	xMat.x = (rcClient.right >> 1) - (SIZE_BUTTON.x >> 1);
+	xClr.x = rcClient.right - SIZE_MARGIN.x - SIZE_BUTTON.x;
 
-	xCat.y = xMat.y = xClr.y = rcClient.bottom - MARGIN_SIZE.y - BUTTON_SIZE.y;
+	xCat.y = xMat.y = xClr.y = rcClient.bottom - SIZE_MARGIN.y - SIZE_BUTTON.y;
 }
 
-static LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+#define log_if(condition)							\
+	if(condition) do {								\
+		OutputDebugString(TEXT(#condition "\n"));	\
+		printf("%s\n", #condition);			\
+	} while(0)
+
+#define fail_with_val_if(val, condition)			\
+	if(condition) do {								\
+		OutputDebugString(TEXT(#condition "\n"));	\
+		printf("%s\n", #condition);					\
+		return (val);								\
+	} while(0)
+
+#define log_if_failed(condition)	log_if(0 > (int32_t)(condition))
+#define fail_if(condition)			fail_with_val_if(-1, condition)
+#define fail_if_failed(condition)	fail_if(0 > (int32_t)(condition))
+#define exit_if_failed(condition)	fail_with_val_if(EXIT_FAILURE, condition)
+
+static int32_t imageAdd(HINSTANCE hInstance, HIMAGELIST hList, const TCHAR* filename, int & out_imageIndex) {
+	HBITMAP hBitmap = LoadBitmap(hInstance, filename); 
+	fail_if(0 == hBitmap);
+	out_imageIndex = ImageList_Add(hList, hBitmap, (HBITMAP)NULL); 
+	DeleteObject(hBitmap); 
+	return (out_imageIndex >= 0) ? 0 : -1;
+}
+
+static int32_t createTreeViewImages(App & app) { 
+	HIMAGELIST hList = 0;
+	constexpr int NUM_BITMAPS   = 1;
+	fail_if(0 == (hList = ImageList_Create(SIZE_ICON.x, SIZE_ICON.y, FALSE, NUM_BITMAPS, 0)));
+
+	fail_if_failed(imageAdd(app.hInstance, hList, TEXT("CategoryEmpty.bmp")	, app.Images.CategoryEmpty));
+	fail_if_failed(imageAdd(app.hInstance, hList, TEXT("CategoryOpen.bmp")	, app.Images.CategoryOpen));
+	fail_if_failed(imageAdd(app.hInstance, hList, TEXT("CategoryClosed.bmp"), app.Images.CategoryClosed));
+	fail_if_failed(imageAdd(app.hInstance, hList, TEXT("Material.bmp")		, app.Images.Material));
+	
+	fail_if(ImageList_GetImageCount(hList) < 4); // Fail if not all of the images were added. 
+
+	TreeView_SetImageList(app.hTree, hList, TVSIL_NORMAL); // Associate the image list with the tree-view control. 
+	return 0;
+}
+
+//static int32_t refreshTree(App & app) { 
+//	(void)app;
+//	//HTREEITEM hPrev			= (HTREEITEM)TVI_FIRST; 
+//	//HTREEITEM hPrevRootItem	= NULL; 
+//	//HTREEITEM hPrevLev2Item	= NULL; 
+//	return 0;
+//}
+
+enum class INPUT_FIELD : uint8_t {
+	Material,
+	Category
+};
+
+static int32_t hideInput(App & app) {
+	SetWindowText(app.hInput, TEXT(""));
+	ShowWindow(app.hInput, SW_HIDE);
+	ShowWindow(app.hAddCategory, SW_SHOW);
+	ShowWindow(app.hAddMaterial, SW_SHOW);
+	return 0;
+}
+
+static int32_t showInput(App & app, INPUT_FIELD	field) {
+	fail_if_failed(::hideInput(app));
+
+	HWND hButton = (field == ::INPUT_FIELD::Material) ? app.hAddMaterial : app.hAddCategory;
+	RECT rcClient = {};
+	GetClientRect(app.hRoot, &rcClient);
+
+	n2u32 posCat = {}, posMat = {}, posClr = {};
+	::calcButtonsPosition(rcClient, posCat, posMat, posClr);
+
+	n2u32 pos = (field == ::INPUT_FIELD::Material) ? posMat : posCat;
+	SetWindowPos(app.hInput, 0, pos.x, pos.y, SIZE_BUTTON.x, SIZE_BUTTON.y, 0);
+	UpdateWindow(app.hInput);
+
+	ShowWindow(hButton, SW_HIDE);
+	ShowWindow(app.hInput, SW_SHOW);
+	SetFocus(app.hInput);
+	return 0;
+}
+
+#ifdef UNICODE
+static int wcstombs (std::string & output, const std::wstring & input)	{
+	if(0 == input.size())
+		return 0;
+
+	int sizeNeededForMultiByte = WideCharToMultiByte(CP_UTF8, 0, &input[0], (int)input.size(), 0, 0, 0, 0);
+	fail_if(0 == sizeNeededForMultiByte);
+
+	std::string converted;
+	converted.resize(sizeNeededForMultiByte);
+	WideCharToMultiByte(CP_UTF8, 0, &input[0], (int)input.size(), &converted[0], (int)converted.size(), 0, 0);
+	output.append(converted);
+	return sizeNeededForMultiByte;
+}
+#endif
+
+static LRESULT CALLBACK EditProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, UINT_PTR /*uIdSubclass*/, DWORD_PTR dwRefData) {
+	App & app = *(App*)dwRefData;
+	switch(uMsg) {
+	case WM_CHAR:
+		switch(wParam) {
+		case VK_ESCAPE:	
+			log_if_failed(::hideInput(app));
+			return 0;
+		case VK_RETURN: {
+			size_t length = GetWindowTextLength(hWnd);
+			if(0 == length)
+				app.NewName.clear();
+			else {
+				app.NewName.resize(length + 1);
+				GetWindowText(hWnd, &app.NewName[0], (int)app.NewName.size());
+			}
+#ifdef UNICODE
+			std::string newName		= {};
+			::wcstombs(newName, app.NewName);
+#else
+			std::string & newName	= app.NewName;
+#endif
+			log_if_failed(app.Tree.AddCategory(newName));
+			log_if_failed(::hideInput(app));
+			return 0;
+		}
+		}
+	}
+	return DefSubclassProc(hWnd, uMsg, wParam, lParam);
+}
+
+static int32_t createTree(App & app, RECT & rcClient) {
+	RECT rcTree = {};
+	::calcTreeRect(rcClient, rcTree);
+
+	constexpr uint32_t STYLE_TREE = WS_VISIBLE | WS_CHILD | WS_BORDER | TVS_HASLINES;
+	app.hTree = CreateWindow(WC_TREEVIEW, TEXT("Tree View"), STYLE_TREE, rcTree.left, rcTree.top, rcTree.right - rcTree.left, rcTree.bottom - rcTree.top, app.hRoot, 0, app.hInstance, 0);
+	return app.hTree ? 0 : -1;
+}
+
+template<typename Tn2>
+static HWND createButton(App & app, HWND parent, Tn2 pos, const TCHAR * text) {
+	constexpr uint32_t STYLE_BUTTON	= WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON;
+	return CreateWindow(TEXT("BUTTON"), text, STYLE_BUTTON, pos.x, pos.y, SIZE_BUTTON.x, SIZE_BUTTON.y, parent, 0, app.hInstance, 0);
+}
+
+static int32_t createButtons(App & app, RECT & rcClient) {
+	n2u32 posCat = {}, posMat = {}, posClr = {};
+	::calcButtonsPosition(rcClient, posCat, posMat, posClr);
+	fail_if(0 == (app.hAddCategory	= ::createButton(app, app.hRoot, posCat, TEXT("New category"))));
+	fail_if(0 == (app.hAddMaterial	= ::createButton(app, app.hRoot, posMat, TEXT("New material"))));
+	fail_if(0 == (app.hClear		= ::createButton(app, app.hRoot, posClr, TEXT("Clear tree"))));
+
+	constexpr uint32_t STYLE_EDIT	= WS_BORDER | WS_CHILD | WS_VISIBLE | ES_LEFT;// | WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL;
+	app.hInput			= CreateWindowEx(0, TEXT("EDIT"), TEXT(""), STYLE_EDIT, posCat.x, posCat.y, SIZE_BUTTON.x * 2, SIZE_BUTTON.y, app.hRoot, 0, app.hInstance, 0);
+	SetWindowSubclass(app.hInput, &EditProc, 0, (DWORD_PTR)&app);
+	ShowWindow(app.hInput, SW_HIDE);
+	return (app.hAddCategory && app.hAddMaterial && app.hClear && app.hInput) ? 0 : -1;
+}
+
+static int32_t createWindow(App & app, RECT & rcClient) {
+	RECT rcWindow = rcClient;  // dimensions of client area 
+	AdjustWindowRect(&rcWindow, WS_OVERLAPPEDWINDOW, 0);
+
+	constexpr uint32_t STYLE_ROOT = WS_VISIBLE | WS_OVERLAPPEDWINDOW;
+	fail_if(0 == (app.hRoot = CreateWindow(app.WndClass.lpszClassName, TEXT("Caption"), STYLE_ROOT, CW_USEDEFAULT, CW_USEDEFAULT, rcWindow.right - rcWindow.left, rcWindow.bottom - rcWindow.top, 0, 0, app.hInstance, &app)));
+	return 0;
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int /*nCmdShow*/) {
+	// Ensure that the common control DLL is loaded. 
+	INITCOMMONCONTROLSEX icc = {sizeof(INITCOMMONCONTROLSEX), ICC_WIN95_CLASSES};
+	InitCommonControlsEx(&icc); 
+
+	App	app	= {hInstance};
+
+	exit_if_failed(0 == RegisterClassEx(&app.WndClass));
+
+	RECT rcClient = {0, 0, 800, 600};  // dimensions of client area 
+
+	exit_if_failed(::createWindow (app, rcClient));
+	exit_if_failed(::createTree   (app, rcClient));
+	exit_if_failed(::createButtons(app, rcClient));
+	exit_if_failed(::createTreeViewImages(app));
+
+	//exit_with_error_if(0 > ::createInput  (app, rcClient));
+
+	MSG msg		= {};
+	msg.wParam	= EXIT_FAILURE;
+	while(GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return int(msg.wParam);
+}
+
+LRESULT CALLBACK hd::WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	switch(uMsg) {
 	case WM_GETMINMAXINFO:	// This message prevents the window from becoming too small.
 		((::MINMAXINFO*)lParam)->ptMinTrackSize	= {320, 200};
 		return 0;
 	case WM_NCCREATE: { 
-		const CREATESTRUCTA						& createStruct			= *(const CREATESTRUCTA*)lParam;
+		const CREATESTRUCTA	& createStruct	= *(const CREATESTRUCTA*)lParam;
 		if(0 == createStruct.hwndParent) {
 			SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)createStruct.lpCreateParams);
 		}
@@ -34,97 +230,32 @@ static LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 	if(0 == app)
 		return DefWindowProc(hWnd, uMsg, wParam, lParam);
 
-	if(hWnd == app->hTree) {}
-	else if(hWnd == app->hAddCategory) {}
-	else if(hWnd == app->hAddMaterial) {}
-	else if(hWnd == app->hClear) {}
-	else if(hWnd == app->hInput) {}
-	else if(hWnd == app->hRoot) {
-		switch(uMsg) {
-		case WM_DESTROY:
-			PostQuitMessage(0);
-			return 0;
-		case WM_SIZE: {
-			RECT rcClient = {};  // dimensions of client area 
-			GetClientRect(app->hRoot, &rcClient);
+	switch(uMsg) {
+	case WM_COMMAND: {
+			 if((HWND)lParam == app->hAddCategory) { ::showInput(*app, INPUT_FIELD::Category); }
+		else if((HWND)lParam == app->hAddMaterial) { ::showInput(*app, INPUT_FIELD::Material); }
+		else if((HWND)lParam == app->hInput) {}
+		break;
+	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_SIZE: {
+		RECT rcClient = {};  // dimensions of client area 
+		GetClientRect(app->hRoot, &rcClient);
 
-			n2u32	posCat = {}, posMat = {}, posClr = {};
-			RECT	rcTree = {};
+		n2u32	posCat = {}, posMat = {}, posClr = {};
+		::calcButtonsPosition(rcClient, posCat, posMat, posClr);
+		SetWindowPos(app->hAddCategory	, 0, posCat.x, posCat.y, SIZE_BUTTON.x, SIZE_BUTTON.y, 0);
+		SetWindowPos(app->hAddMaterial	, 0, posMat.x, posMat.y, SIZE_BUTTON.x, SIZE_BUTTON.y, 0);
+		SetWindowPos(app->hClear		, 0, posClr.x, posClr.y, SIZE_BUTTON.x, SIZE_BUTTON.y, 0);
+		SetWindowPos(app->hInput		, 0, posMat.x, posMat.y, SIZE_BUTTON.x, SIZE_BUTTON.y, 0);
 
-			::calcButtonsPosition(rcClient, posCat, posMat, posClr);
-			::calcTreeRect(rcClient, rcTree);
-
-			SetWindowPos(app->hAddCategory	, 0, posCat.x, posCat.y, BUTTON_SIZE.x, BUTTON_SIZE.y, 0);
-			SetWindowPos(app->hAddMaterial	, 0, posMat.x, posMat.y, BUTTON_SIZE.x, BUTTON_SIZE.y, 0);
-			SetWindowPos(app->hClear		, 0, posClr.x, posClr.y, BUTTON_SIZE.x, BUTTON_SIZE.y, 0);
-			SetWindowPos(app->hTree			, 0, rcTree.left, rcTree.top, rcTree.right - rcTree.left, rcTree.bottom - rcTree.top, 0);
-			break;
-		}
-		}
+		RECT	rcTree = {};
+		::calcTreeRect(rcClient, rcTree);
+		SetWindowPos(app->hTree, 0, rcTree.left, rcTree.top, rcTree.right - rcTree.left, rcTree.bottom - rcTree.top, 0);
+		break;
+	}
 	}
 	return DefWindowProc(hWnd, uMsg, wParam, lParam);
-}
-
-static void defaultWndClass(WNDCLASSEX & wndClass, HINSTANCE hInstance) {
-	wndClass				= {sizeof(WNDCLASSEX)};
-
-	wndClass.lpfnWndProc	= WndProc;
-	wndClass.hInstance		= hInstance;
-	wndClass.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wndClass.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wndClass.lpszClassName	= TEXT("HD_Window");
-	wndClass.hIcon			= LoadIcon(NULL, IDI_APPLICATION);
-	wndClass.hIconSm		= LoadIcon(NULL, IDI_APPLICATION);
-}
-
-HWND createButton(App & app, n2u32 pos, const TCHAR * text) {
-	constexpr uint32_t STYLE_BUTTON	= WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON;
-	return CreateWindow(TEXT("BUTTON"), text, STYLE_BUTTON, pos.x, pos.y, BUTTON_SIZE.x, BUTTON_SIZE.y, app.hRoot, 0, app.hInstance, 0);
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int /*nCmdShow*/) {
-    // Ensure that the common control DLL is loaded. 
-    INITCOMMONCONTROLSEX icc = {sizeof(INITCOMMONCONTROLSEX), ICC_WIN95_CLASSES};
-    InitCommonControlsEx(&icc); 
-
-	WNDCLASSEX wndClass;
-	defaultWndClass(wndClass, hInstance);
-	if(!RegisterClassEx(&wndClass)) {
-		MessageBox(0, TEXT("Window Registration Failed!"), TEXT("Error!"), MB_ICONEXCLAMATION|MB_OK);
-		return EXIT_FAILURE;
-	}
-
-	App		app			= {hInstance};
-
-	RECT	rcClient	= {0, 0, 800, 600};  // dimensions of client area 
-	RECT	rcWindow	= rcClient;  // dimensions of client area 
-	AdjustWindowRect(&rcWindow, WS_OVERLAPPEDWINDOW, 0);
-	constexpr uint32_t STYLE_ROOT	= WS_VISIBLE | WS_OVERLAPPEDWINDOW;
-	app.hRoot = CreateWindow(wndClass.lpszClassName, TEXT("Caption"), STYLE_ROOT, CW_USEDEFAULT, CW_USEDEFAULT, rcWindow.right - rcWindow.left, rcWindow.bottom - rcWindow.top, 0, 0, hInstance, &app);
-
-	if(app.hRoot == NULL) {
-		MessageBox(0, TEXT("Window Creation Failed!"), TEXT("Error!"), MB_ICONEXCLAMATION | MB_OK);
-		return EXIT_FAILURE;
-	}
-	UpdateWindow(app.hRoot);
-
-	RECT rcTree = {};
-	n2u32 posCat = {}, posMat = {}, posClr = {};
-	::calcButtonsPosition(rcClient, posCat, posMat, posClr);
-	::calcTreeRect(rcClient, rcTree);
-
-	app.hAddCategory	= ::createButton(app, posCat, TEXT("New category"));
-	app.hAddMaterial	= ::createButton(app, posMat, TEXT("New material"));
-	app.hClear			= ::createButton(app, posClr, TEXT("Clear tree"));
-
-	constexpr uint32_t STYLE_TREE = WS_VISIBLE | WS_CHILD | WS_BORDER | TVS_HASLINES;
-	app.hTree			= CreateWindow(WC_TREEVIEW, TEXT("Tree View"), STYLE_TREE, rcTree.left, rcTree.top, rcTree.right - rcTree.left, rcTree.bottom - rcTree.top, app.hRoot, 0, hInstance, 0); 
-
-	MSG msg		= {};
-	msg.wParam	= EXIT_FAILURE;
-	while(GetMessage(&msg, NULL, 0, 0) > 0) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg); 
-	}
-	return int(msg.wParam);
 }
--- hdtreeapp/hdtreeapp.h
+++ hdtreeapp/hdtreeapp.h
@@ -13,17 +13,56 @@ namespace hd
 	typedef n2<uint32_t> n2u32;
 	typedef n2<long>	 n2lng;
 
-	static constexpr n2u32 BUTTON_SIZE = {96, 24};
-	static constexpr n2lng MARGIN_SIZE = {2, 2};
+	static constexpr n2u32 SIZE_ICON	= {16, 16};
+	static constexpr n2u32 SIZE_BUTTON	= {96, 24};
+	static constexpr n2lng SIZE_MARGIN	= {2, 2};
 
+	// Holds 
+	struct TVImages {
+	    int			Material		= 0;
+	    int			CategoryEmpty	= 0;
+	    int			CategoryOpen	= 0;
+	    int			CategoryClosed	= 0;
+	};
+
+	LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+	
+	static void defaultWndClass(WNDCLASSEX & wndClass, HINSTANCE hInstance) {
+		wndClass				= {sizeof(WNDCLASSEX)};
+
+		wndClass.lpfnWndProc	= WndProc;
+		wndClass.hInstance		= hInstance;
+		wndClass.hCursor		= LoadCursor(NULL, IDC_ARROW);
+		wndClass.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+		wndClass.lpszClassName	= TEXT("HD_Window");
+		wndClass.hIcon			= LoadIcon(NULL, IDI_APPLICATION);
+		wndClass.hIconSm		= LoadIcon(NULL, IDI_APPLICATION);
+	}
+
+	// 
 	struct App {
-		HINSTANCE	hInstance		= {};
-		HWND		hRoot			= {};
-		HWND		hTree			= {};
-		HWND		hInput			= {};
-		HWND		hAddCategory	= {};
-		HWND		hAddMaterial	= {};
-		HWND		hClear			= {};
+		HINSTANCE		hInstance		= {};
+		HWND			hRoot			= {};
+		HWND			hTree			= {};
+		HWND			hAddCategory	= {};
+		HWND			hAddMaterial	= {};
+		HWND			hClear			= {};
+		HWND			hInput			= {};
+
+		TVImages		Images			= {};
+
+		MaterialTree	Tree;
+		
+		WNDCLASSEX		WndClass		= {};
+#ifdef UNICODE
+		std::wstring	NewName			= {};
+#else
+		std::string		NewName			= {};
+#endif
+		App	(HINSTANCE _hInstance) 
+			: hInstance(_hInstance) { 
+			defaultWndClass(WndClass, hInstance); 
+		}
 	};
 } // namespace
 
